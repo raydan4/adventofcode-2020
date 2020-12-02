613f73dc7c2a85a0f43443f37c88ac874fdc3947
raydan4 <dszafran116@gmail.com>
2020-12-01 23:56:03 -0500

    day 1, maybe I'll make it better

---
+++ day1/day1.py
@@ -0,0 +1,29 @@
+from requests import get
+
+FILE = 'day1.input'
+
+def part1(numset):
+  for i in numset:
+    if j := 2020 - i in numset:
+      return i * j
+  return None
+
+
+def part2(numset):
+  for i in numset:
+    val = 2020 - i
+    for j in numset:
+      if k := val - j in numset:
+        return i * j * k
+  return None
+
+
+if __name__ == '__main__':
+  numset = set()
+  with open(FILE) as f:
+    for line in f:
+      numset.add(int(line))
+
+  print(part1(numset))
+  print(part2(numset))
+
