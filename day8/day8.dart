cb8bb96b0fefeb2e3d76529cb93e25faae1ee6a9
raydan4 <dszafran116@gmail.com>
2020-12-10 06:44:38 -0500

    Finished day 8 :)

--- day8/day8.dart
+++ day8/day8.dart
@@ -11,24 +11,56 @@ int main() {
   int acc = 0;
   int pc = 0;
 
+  // Data for program correction
+  Queue candidates = new Queue();
+  for (var i = 0; i < instructions.length; i++) {
+    if (instructions[i].startsWith("nop") || instructions[i].startsWith("jmp")) {
+      candidates.add(i);
+    }
+  }
+  int candidate = -1;
+
   while(true) {
+    if (ran.containsKey(pc) || pc < 0 || pc > instructions.length - 1) {
+      // Only print challenge 1 result once
+      if (candidate == -1) { print("Challenge 1: $acc"); }
+
+      // Clear execution variables
+      ran.clear();
+      acc = 0;
+      pc = 0;
+
+      // Update modification candidate
+      candidates.removeFirst();
+      candidate = candidates.first;      
+    }
+    // If command is last line of input program ran successfully
+    if (pc == instructions.length -1) { print("Challenge 2: $acc"); return 0; }
+
     // Get current instruction
     dynamic instruction = instructions[pc];
     
-    if (ran.containsKey(pc)) { print("Challenge 1: $acc"); }
+    // Note that instruction being ran
     ran[pc] = instruction;
-    
-    dynamic command = instruction.substring(0,3); 
-    dynamic sign = instruction.substring(4,5) == '+' ? 1 : -1;
+  
+    // Split instruction 
+    // Command is first three characters
+    dynamic cmd = instruction.substring(0,3);
+    // Will multiply by 1 if sign is '+', -1 if sign is '-'
+    dynamic sign = instruction[4] == '+' ? 1 : -1;
+    // Value is integer at end
     dynamic val = int.parse(instruction.substring(5,instruction.length));
 
-    switch(command) {
+    // If this is the next line to try flipping nop/jmp, flip the command
+    if (pc == candidate) { cmd = cmd == "jmp" ? "nop": "jmp"; }
+
+    switch(cmd) {
       // Update program counter
       update:
-      case "nop": { pc += 1; }
+      case "nop": { pc ++; }
         break;
       // Update Accumulator
-      case "acc": { acc += val * sign;}
+      case "acc": { acc += val * sign; }
         continue update;
       // Jump program counter
       case "jmp": { pc += val * sign; }
