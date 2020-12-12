80521cc810b7f4e4cae6945450148af619bd2bc7
raydan4 <dszafran116@gmail.com>
2020-12-12 05:41:07 -0500

    Improved code and finished challenge 2

--- day11/day11.nim
+++ day11/day11.nim
@@ -1,45 +1,76 @@
-import streams, strutils, sequtils, sugar
+import streams, strutils, strformat, sequtils, sugar
 
-func count_occupied(rows: seq[string]): int =
+
+func count_occupied(state: seq[string]): int =
   var count = 0
-  for row in rows:
+  for row in state:
     count += len(row.filter(x => x == '#'))
   count
 
+
+func in_range(pos: int, min: int, max: int): bool =
+  min <= pos and max >= pos
+
+
+proc next_seat_full(state: seq[string], i: int, j: int, h: int, w: int, limit: bool): bool =
+  let rmax = len(state) - 1
+  let cmax = len(state[i]) - 1
+
+  var row = i + h
+  var col = j + w
+
+  while in_range(row, 0, rmax) and in_range(col, 0, cmax):
+    let c = state[row][col]
+    if c == '#': return true
+    if c == 'L': return false
+    if limit: break
+    row += h
+    col += w
+  false 
+
+
+proc count_adjacent(state: seq[string], i: int, j: int, limit: bool): int =
+  var count  = 0
+  for h in -1..1:
+    for w in -1..1:
+      if h == 0 and w == 0: continue
+      count += int(next_seat_full(state, i, j, h, w, limit))
+  count
+
+
+proc calc_round(state: seq[string], maxadjacent: int, limit: bool): seq[string] =
+  var new = state
+  
+  for i, row in state:
+    for j, col in row:
+      let sum = count_adjacent(state, i, j, limit)
+      if col == '#' and sum >= maxadjacent: new[i][j] = 'L'
+      if col == 'L' and sum == 0: new[i][j] = '#'
+  new
+
+
+proc simulate(state: seq[string], maxadjacent: int, limit: bool): int =
+  var new = state
+  var curr = newSeq[string]()
+
+  while new != curr:
+    curr = new
+    new = calc_round(curr, maxadjacent, limit)
+  count_occupied(curr)
+
+
+# Main
 var strm = newFileStream("day11.input", fmRead)
 var line = ""
-var rows = newSeq[string]()
-var prev = newSeq[string]()
+var start = newSeq[string]()
 
 if not isNil(strm):
   while strm.readLine(line):
-    if not isEmptyOrWhitespace(line): rows.add(line.strip())
+    if not isEmptyOrWhitespace(line): start.add(line.strip())
   strm.close()
-  
-let maxh = len(rows) - 1
-let maxw = len(rows[0]) - 1
-let minh = 0
-let minw = 0
 
-while prev != rows:
-  prev = rows
-
-  for i, row in prev:
-    for j, chr in row:
-      var sum  = 0
-
-      if              j > minw and prev[i  ][j-1] == '#': sum += 1  # left
-      if i > minh and j > minw and prev[i-1][j-1] == '#': sum += 1  # upper left
-      if i > minh              and prev[i-1][j  ] == '#': sum += 1  # upper
-      if i > minh and j < maxw and prev[i-1][j+1] == '#': sum += 1  # upper right
-      if              j < maxw and prev[i  ][j+1] == '#': sum += 1  # right
-      if i < maxh and j < maxw and prev[i+1][j+1] == '#': sum += 1  # lower right
-      if i < maxh              and prev[i+1][j  ] == '#': sum += 1  # lower
-      if i < maxh and j > minw and prev[i+1][j-1] == '#': sum += 1  # lower left
-
-      if chr == '#' and sum >= 4: rows[i][j] = 'L'
-      if chr == 'L' and sum == 0: rows[i][j] = '#'
-
-let challenge1 = count_occupied(rows)
-
+let challenge1 = simulate(start, 4, true)
 echo "Challenge 1: ", challenge1
+
+let challenge2 = simulate(start, 5, false)
+echo "Challenge 2: ", challenge2
